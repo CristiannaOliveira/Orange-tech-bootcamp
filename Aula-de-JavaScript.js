fb91ffc42ad150740c3b6974f8d072cd35f693e5
Cristianna dos Santos Oliveira <118199718+CristiannaOliveira@users.noreply.github.com>
2022-11-17 15:02:51 -0300

    Create Aula-de-JavaScript.js

---
+++ Aula-de-JavaScript.js
@@ -0,0 +1,21 @@
+
+let aula1 = 10;
+console.log(aula1);
+
+aula1 =20;
+
+const pi = 3.14;
+
+console.log(aula1);
+
+let aula12 = 10 + 15;
+console.log(aula12);
+
+let aula13 = 10 - 2;
+console.log(aula13);
+
+let aula14 = 10 * 3;
+console.log(aula14);
+
+let aula15 = 10/5;
+console.log(aula15);
