7f293c1c5bc4db0d6ea80cc7927e720f65f76162
Cristianna dos Santos Oliveira <118199718+CristiannaOliveira@users.noreply.github.com>
2022-11-17 22:37:09 -0300

    Adição
    Adição de modificações.

---
+++ 02 - Condicionais/aula2.js
@@ -0,0 +1,13 @@
+/* true e false é Boolean. */
+
+
+const numero = 2001;
+const numeroResultado = (numero/2);
+console.log(numeroResultado);
+
+if (numeroResultado === 0) {
+    console.log('Par');
+}
+else {
+    console.log('Ímpar');
+}
