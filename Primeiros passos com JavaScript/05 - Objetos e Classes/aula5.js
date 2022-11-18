a90726a6ab36646e9dfad9210af704ba2ba82c69
Cristianna dos Santos Oliveira <118199718+CristiannaOliveira@users.noreply.github.com>
2022-11-18 19:18:14 -0300

    Create aula5.js

---
+++ Primeiros passos com JavaScript/05 - Objetos e Classes/aula5.js
@@ -0,0 +1,21 @@
+/* Um objeto no JavaScript é uma coleção de valores 
+*/
+
+const cristianna = {
+    nome: 'Cristianna dos Santos Oliveira',
+    idade: 21
+    
+}
+
+console.log(cristianna.nome);
+console.log(cristianna.idade);
+console.log(cristianna);
+
+delete cristianna.nome;
+
+cristianna.altura = 1.58;
+
+console.log(cristianna);
+
+cristianna.descrever ();
+
