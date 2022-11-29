4b12e34d2a2be1ece890da9ed269fa0050fde36a
Cristianna dos Santos Oliveira <118199718+CristiannaOliveira@users.noreply.github.com>
2022-11-29 00:06:51 -0300

    importação e exportação exerc.

---
+++ Primeiros passos com JavaScript/07 - Exportação e Importação/exercício1.js
@@ -0,0 +1,34 @@
+
+const {gets, print} = require('./funcoes-auxiliares');
+
+/* Uma sala contém 5 alunos e para cada aluno foi sorteado um número de 1-100.
+   Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado.
+
+   Dados de Entrada:
+   5 
+   50
+   10
+   98
+   23
+
+   Saída:
+   98
+   */
+
+   print(gets());
+   print(gets());
+   print(gets());
+   print(gets());
+   print(gets());
+
+   const numeros = gets();
+   let maiorValorEncontrado = 0;
+
+   for (let i = 0; i < numeros; i++) {
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado;
+    }
+}
+
+print(maiorValorEncontrado);
