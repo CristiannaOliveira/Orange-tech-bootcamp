ba23baf10821c42fa9cc1d7d91c62cbb2a172a32
Cristianna dos Santos Oliveira <118199718+CristiannaOliveira@users.noreply.github.com>
2022-12-01 17:32:26 -0300

    aula

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -5,16 +5,12 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 /*o Then é para o sucesso e caso dê erro é o Catch.Se eu quiser algo apesar do sucesso ou do fracasso
 é o Finally. */ 
-
+// o => serve para reduzir a sintaxe de função, com uma linha eu posso apagar o return.
 fetch(url)
-.then(function (response){
-console.log(response);
-})
+.then((response) => response.json())
 
-.catch(function(error){
-console.error(error);
-})
+.then((jsonBody) => console.log(jsonBody))
 
-.finally(function(){
-console.log('Requisição concluída!');
-})
+.catch((error) => console.error(error))
+
+.finally((frase) =>console.log('Requisição concluída!'))
---
+++ Primeiros passos com JavaScript/teste.js
@@ -0,0 +1,9 @@
+
+ 
+var a = 5;
+var N = 20;
+
+for (let i= 0; i => N; i*5){
+  const múltiplos = a[i];
+  console.log(múltiplos);
+}
