089c0a0077f248d41f2b216085c2a335e60e2bf4
Cristianna dos Santos Oliveira <118199718+CristiannaOliveira@users.noreply.github.com>
2022-12-01 14:38:21 -0300

    Adições no JS

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -1,5 +1,20 @@
 
-            
-            
-            
-        
+const offset = 0;        
+const limit = 10;       
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`       
+
+/*o Then é para o sucesso e caso dê erro é o Catch.Se eu quiser algo apesar do sucesso ou do fracasso
+é o Finally. */ 
+
+fetch(url)
+.then(function (response){
+console.log(response);
+})
+
+.catch(function(error){
+console.error(error);
+})
+
+.finally(function(){
+console.log('Requisição concluída!');
+})
